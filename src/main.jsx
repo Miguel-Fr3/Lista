c64a915cf1a19636b000873119778643740a72b5
Miguel-Fr3 <rm99997@fiap.com.br>
2023-09-02 12:19:33 -0300

    Erros novamente

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './routes/Home/index.jsx'
 import Produtos from './routes/Produtos/index.jsx'
-import Error from './routes/Error/index.jsx'
+import Error from './routes/Error/Index.jsx'
 import EditarProduto from './routes/EditarProdutos/index.jsx'
 
 const router = createBrowserRouter([
