3cbd6ea28771062a76b1c67a8f238bf534ef771f
Miguel-Fr3 <rm99997@fiap.com.br>
2023-09-02 12:12:52 -0300

    Criando links para navegar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import Menu from './Components/Menu'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Menu/>
       <Outlet/>
     </>
   )
